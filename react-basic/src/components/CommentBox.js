18d6d6c6eb6f7791f2dc189604c45faf9b66c0c7
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2020-01-16 14:25:23 +0700

    'update'

--- react-basic/src/components/CommentBox.js
+++ react-basic/src/components/CommentBox.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
-const CommentBox = () => (
-  <>
-    <div className="form-group">
-      <textarea className="form-control"></textarea>
-    </div>
-    <div className="form-group">
-      <button className="btn btn-primary">Submit</button>
-    </div>
-  </>
-);
+const CommentBox = () => {
+  const [currentComment, setCurrentComment] = useState("");
+  const [comments, setComments] = useState([]);
+
+  const handleTextChange = e => {
+    setCurrentComment(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    setComments([...comments, currentComment]);
+    setCurrentComment("");
+  };
+
+  return (
+    <>
+      <div className="form-group">
+        <textarea
+          value={currentComment}
+          onChange={handleTextChange}
+          className="form-control"
+        ></textarea>
+      </div>
+      <div className="form-group">
+        <button onClick={handleSubmit} className="btn btn-primary">
+          Submit
+        </button>
+      </div>
+      {comments.map(v => (
+        <div className="alert alert-primary" role="alert">
+          {v}
+        </div>
+      ))}
+    </>
+  );
+};
 export default CommentBox;
